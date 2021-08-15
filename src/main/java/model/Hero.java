00bace44c59415beb0155b64d34ec8c6c476bfba
getty <gcherono15@gmail.com>
2021-08-15 22:03:49 +0300

    added code to check if hero added exists

--- src/main/java/model/Hero.java
+++ src/main/java/model/Hero.java
@@ -1,8 +1,6 @@
 package model;
-
-import java.security.PrivateKey;
 import java.util.ArrayList;
-import java.util.List;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Hero {
 
@@ -11,25 +9,49 @@ public class Hero {
     private String specialPower;
     private String weakness;
     private String yourSquad;
+    private boolean check = false;
 
     private static ArrayList<Hero> instances = new ArrayList<>();
 
-    public Hero(String name, Integer age, String specialPower, String weakness) {
-    public Hero(String name, Integer age, String specialPower, String weakness, String yourSquad) {
-            this.name = name;
-            this.specialPower = specialPower;
-            this.weakness = weakness;
-            this.age = age;
-            this.yourSquad = yourSquad;
+    public Hero(String name, Integer age, String specialPower, String weakness, String yourSquad){
+        this.name = name;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+        this.age = age;
+        this.yourSquad = yourSquad;
+
+        for (int i = 0 ; i < instances.size() ; i++){
+            if (instances.get(i).getName().equals(name)){
+                showMessageDialog(null, "The Hero exists in one of the Squads");
+                check = true;
+            }
+        }
+        if (!check){
             instances.add(this);
         }
+    }
 
-        public String getYourSquad () {
-            return yourSquad;
-        }
+    public String getYourSquad() {
+        return yourSquad;
+    }
 
-        public Integer getAge () {
-            return age;
-        }
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public static ArrayList<Hero> getAll() {
+        return instances;
     }
 }
