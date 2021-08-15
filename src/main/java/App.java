16601fa13e1eb38aaba0c02eadb47f13c2e897b7
getty <gcherono15@gmail.com>
2021-08-15 17:40:46 +0300

    added a code to add or view heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
 import model.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -10,36 +11,46 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         //showMessageDialog(null,"This is an alert!!");
-        staticFileLocation("/public");
+        public static void main(String[] args) { //type “psvm + tab” to autocreate this
+            staticFileLocation("/public");
 
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Hero> tasks = Hero.getAll();
-            model.put("tasks", tasks);
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+            //get: show all tasks
+            get("/", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                ArrayList<Hero> tasks = Hero.getAll();
+                model.put("tasks", tasks);
+                ArrayList<Hero> heros = Hero.getAll();
+                model.put("heros", heros);
+                return new ModelAndView(model, "index.hbs");
+            }, new HandlebarsTemplateEngine());
 
-        //get: show new task form
-        get("/form", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "heroForm.hbs");
-        }, new HandlebarsTemplateEngine());
+            //get: show new task form
+            //get: show new Hero form
+            get("/form", (req, res) -> {
+                Map<String, Object> model = new HashMap<>();
+                return new ModelAndView(model, "heroForm.hbs");
+            }, new HandlebarsTemplateEngine());
 
-        //task: process new task form
-        post("/tasks", (req, res) -> { //URL to make new task on POST route
-            Map<String, Object> model = new HashMap<>();
-            String name = req.queryParams("name");
-            String ageString = req.queryParams("age");
-            Integer age = Integer.parseInt(ageString);
-            String specialPower = req.queryParams("specialPower");
-            String weakness = req.queryParams("weakness");
-            Hero newHero = new Hero(name,age,specialPower,weakness);
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
+            //task: process new task form
+            //task: process new Hero form
+            post("/tasks", (req, res) -> { //URL to make new task on POST route
+                Map<String, Object> model = new HashMap<>();
+                String name = req.queryParams("name");
+                String ageString = req.queryParams("age");
+                Integer age = Integer.parseInt(ageString);
+                String specialPower = req.queryParams("specialPower");
+                String weakness = req.queryParams("weakness");
+                String stringAge = req.queryParams("age");
+                Integer age = Integer.parseInt(stringAge);
+                String specialPower = req.queryParams("name");
+                String weakness = req.queryParams("name");
+                Hero newHero = new Hero(name,age,specialPower,weakness);
+                res.redirect("/");
+                return null;
+            }, new HandlebarsTemplateEngine());
 
-        //get: show all tasks
+            //get: show all tasks
 
 
+        }
     }
-}
